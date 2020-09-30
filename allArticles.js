7403281ccd16ce6ab236a33dd07ffe3a5601cc66
theCoder505 <68850204+theCoder505@users.noreply.github.com>
2020-10-01 01:06:26 +0600

    added new files

---
+++ allArticles.js
@@ -0,0 +1,38 @@
+
+
+document.querySelector(".show1").addEventListener("click", function(){
+    document.querySelector(".show1").style.display = "none";
+    document.querySelector(".secondShow").style.display =  "-webkit-box";
+
+});
+
+
+
+document.querySelector(".show2").addEventListener("click", function(){
+    document.querySelector(".show2").style.display = "none";
+    document.querySelector(".thirdShow").style.display =  "-webkit-box";
+
+});
+
+
+
+document.querySelector(".show3").addEventListener("click", function(){
+    document.querySelector(".show3").style.display = "none";
+    document.querySelector(".fourthShow").style.display =  "-webkit-box";
+
+});
+
+
+
+document.querySelector(".show4").addEventListener("click", function(){
+    document.querySelector(".show4").style.display = "none";
+    document.querySelector(".lastShow").style.display =  "-webkit-box";
+
+});
+
+
+
+
+
+
+
